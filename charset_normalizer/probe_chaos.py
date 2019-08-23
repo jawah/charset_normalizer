a10edb0cf124e22656051e334775a447fbcb4b5b
TAHRI Ahmed R <nyuubi.10@gmail.com>
2019-08-23 18:08:49 +0200

    English translation for unicode ranges / alphabets

--- charset_normalizer/probe_chaos.py
+++ charset_normalizer/probe_chaos.py
@@ -1,16 +1,13 @@
 # coding: utf-8
 import re
 
-from charset_normalizer.unicode import UnicodeRangeIdentify
-
-from dragonmapper.hanzi import identify as s_identify
 from dragonmapper.hanzi import MIXED, BOTH, UNKNOWN
+from dragonmapper.hanzi import identify as s_identify
 from zhon.hanzi import sentence as cjc_sentence_re
 
-try:
-    from functools import lru_cache
-except ImportError:
-    from functools32 import lru_cache
+from charset_normalizer.unicode import UnicodeRangeIdentify
+
+from functools import lru_cache
 
 
 @lru_cache(maxsize=8192)
@@ -66,42 +63,25 @@ class ProbeChaos:
             'encountered_unicode_range_occurrences': self.encountered_unicode_range_occurrences,
             'not_encountered_white_space_reset': self.not_encountered_white_space_reset,
             'total_letter_encountered': self.total_letter_encountered,
-            '_unravel_suspicious_ranges': self._unravel_suspicious_ranges(),
+            '_unravel_suspicious_ranges': UnicodeRangeIdentify.unravel_suspicious_ranges(len(self._string), self.encountered_unicode_range_occurrences),
             'total_lower_letter_encountered': self.total_lower_letter_encountered,
             'total_upper_accent_encountered': self.total_upper_accent_encountered,
             'total_upper_accent_encountered_inner': self.total_upper_accent_encountered_inner,
             'total_unaccented_letter_encountered': self.total_unaccented_letter_encountered,
-            'cjc_id': s_identify(self._string) if 'Idéogrammes unifiés CJC' in self.encountered_unicode_range else 0,
-            'f(x)': '({} + {} + {} + {} + {} + {}) / {}'.format(self.successive_upper_lower, self.successive_accent, self.successive_different_unicode_range, self.not_encountered_white_space, self.unprintable, self._unravel_suspicious_ranges(), len(self._string))
+            'cjc_id': s_identify(self._string) if 'CJK Unified Ideographs' in self.encountered_unicode_range else 0,
+            'f(x)': '({} + {} + {} + {} + {} + {}) / {}'.format(self.successive_upper_lower, self.successive_accent, self.successive_different_unicode_range, self.not_encountered_white_space, self.unprintable, UnicodeRangeIdentify.unravel_suspicious_ranges(len(self._string), self.encountered_unicode_range_occurrences), len(self._string))
         }
-
-    def _unravel_suspicious_ranges(self):
-        len_ = len(self._string)
-        s_ = 0
-
-        for k, v in self.encountered_unicode_range_occurrences.items():
-            k_ = k.lower()
-            if ('latin' not in k_ and 'ponctuation générale' not in k_ and 'formes de demi et pleine chasse' not in k_ and 'symboles et ponct.' not in k_ and 'cjc' not in k_) or 'latin étendu' in k_ or 'supplément latin' in k_:
-                if v / len_ < 0.09:
-                    if len(self.encountered_unicode_range_occurrences.keys()) <= 2 and 'supplément latin' in k_:
-                        continue
-                    # print('Suspicous ranges added', k, v)
-                    s_ += v if 'formes géométriques' not in k_ else v*10
-
-        return s_
 
     def _probe(self):
 
         c__ = False
 
-        # Todo: Gain 200ms by doing this : list(zip(self._string, self._string[1:]))[::2]
-        # OR zip(self._string[::2], self._string[1::2])
         for c, i_ in zip(self._string, range(0, len(self._string))):
-
-            if not c__ and (i_ / len(self._string)) > 0.2 and self.ratio >= 0.3:
+            state_ = (i_ / len(self._string) >= 0.5)
+            if not c__ and state_ > 0.2 and self.ratio >= 0.3:
                 self.gave_up = True
                 break
-            elif c__ is False and (i_ / len(self._string)) > 0.2:
+            elif c__ is False and state_ > 0.2:
                 c__ = True
 
             self.total_letter_encountered += 1
@@ -109,7 +89,7 @@ class ProbeChaos:
             if not c.isprintable():
                 if c not in ['\n', '\t', '\r']:
                     u_name = UnicodeRangeIdentify.find_letter_type(c)
-                    if 'CJC' not in u_name and 'Ponctuation générale' not in u_name and ord(c) != 160:  # CJC have there own space char
+                    if 'CJK' not in u_name and 'General Punctuation' not in u_name and ord(c) != 160:  # CJC have there own white spaces
                         self.unprintable += 2
                 else:
                     self.encountered_white_space += 1
@@ -135,6 +115,10 @@ class ProbeChaos:
             u_name = UnicodeRangeIdentify.find_letter_type(c)
             u_name_lower = u_name.lower() if u_name is not None else None
 
+            is_upper = c.isupper()
+            is_lower = c.islower() if not is_upper else False
+            is_alpha = c.isalpha()
+
             if u_name is not None and u_name not in self.encountered_unicode_range:
                 self.encountered_unicode_range_occurrences[u_name] = 0
                 self.encountered_unicode_range.add(u_name)
@@ -142,26 +126,25 @@ class ProbeChaos:
             if is_accent and UnicodeRangeIdentify.is_accentuated(self.previous_printable_letter):
                 self.successive_accent += 2
 
-            if c.islower():
+            if is_lower:
                 self.total_lower_letter_encountered += 1
 
-            if c.isupper() and is_accent:
+            if is_upper and is_accent:
                 self.total_upper_accent_encountered += 1
-
                 if self.previous_printable_letter.isalpha():
                     self.total_upper_accent_encountered_inner += 1
-            elif not is_accent and c.isalpha():
+            elif not is_accent and is_alpha:
                 self.total_unaccented_letter_encountered += 1
 
             if u_name is not None:
                 self.encountered_unicode_range_occurrences[u_name] += 1
 
-                if 'symboles et ponct.' in u_name_lower or 'ponctuation générale' in u_name_lower:
+                if 'symbols and punctuation' in u_name_lower or 'general punctuation' in u_name_lower:
                     self.encountered_white_space += 1
                     self.not_encountered_white_space = 0
                     self.not_encountered_white_space_reset += 1
 
-                if 'latin' in u_name_lower or 'formes de demi et pleine chasse' in u_name_lower or 'symboles et ponct.' in u_name_lower or 'ponctuation générale' in u_name_lower or u_name is None:
+                if 'latin' in u_name_lower or 'halfwidth and fullwidth forms' in u_name_lower or 'symbols and punctuation' in u_name_lower or 'general punctuation' in u_name_lower:
                     self.previous_printable_letter = c
                     continue
                 elif (self.previous_printable_letter.isupper() and c.islower()) or (
@@ -169,22 +152,22 @@ class ProbeChaos:
                     self.successive_upper_lower += 1
 
                 if u_name != self.previous_encountered_unicode_range and self.previous_encountered_unicode_range is not None:
-                    if 'latin' not in self.previous_encountered_unicode_range.lower() and \
-                            'formes de demi et pleine chasse' not in self.previous_encountered_unicode_range.lower() and \
-                            'symboles et ponct.' not in self.previous_encountered_unicode_range.lower() and \
-                            'ponctuation générale' not in self.previous_encountered_unicode_range.lower():
-                        # print('successive unicode range', u_name, 'with', previous_encountered_unicode_range)
+                    k__ = self.previous_encountered_unicode_range
+                    if 'latin' not in k__ and \
+                            'halfwidth and fullwidth forms' not in k__ and \
+                            'symbols and punctuation' not in k__ and \
+                            'general punctuation' not in k__:
                         self.successive_different_unicode_range += 1
 
             self.previous_encountered_unicode_range = u_name
             self.previous_printable_letter = c
 
     def _unravel_cjc_suspicious(self):
-        if 'Idéogrammes unifiés CJC' in self.encountered_unicode_range:
+        if 'CJK Unified Ideographs' in self.encountered_unicode_range:
             if s_identify(self._string) in [MIXED, BOTH]:
-                return self.encountered_unicode_range_occurrences['Idéogrammes unifiés CJC']
+                return self.encountered_unicode_range_occurrences['CJK Unified Ideographs']
             elif len(re.findall(cjc_sentence_re, self._string)) == 0:
-                return self.encountered_unicode_range_occurrences['Idéogrammes unifiés CJC']
+                return self.encountered_unicode_range_occurrences['CJK Unified Ideographs']
 
         return UNKNOWN
 
@@ -198,5 +181,6 @@ class ProbeChaos:
         :rtype: float
         """
         r_ = self.total_upper_accent_encountered if self.total_letter_encountered > 0 and self.total_unaccented_letter_encountered / self.total_letter_encountered < 0.5 else 0
-        return (r_ + self.successive_upper_lower + self.successive_accent + self.successive_different_unicode_range + self.not_encountered_white_space + self.unprintable + self._unravel_suspicious_ranges() + self._unravel_cjc_suspicious()) / len(self._string)  # + len(self.encountered_unicode_range)-1
+        z_ = UnicodeRangeIdentify.unravel_suspicious_ranges(len(self._string), self.encountered_unicode_range_occurrences)
+        return (r_ + self.successive_upper_lower + self.successive_accent + self.successive_different_unicode_range + self.not_encountered_white_space + self.unprintable + z_ + self._unravel_cjc_suspicious()) / len(self._string)  # + len(self.encountered_unicode_range)-1
 
--- charset_normalizer/unicode.py
+++ charset_normalizer/unicode.py
@@ -1,10 +1,11 @@
 # coding: utf-8
-from charset_normalizer.constant import UNICODE_RANGES_ZIP
-
+from charset_normalizer.constant import UNICODE_RANGES_ZIP, UNICODE_RANGES_NAMES
 from functools import lru_cache
 
 
 class UnicodeRangeIdentify:
+
+    SUSPICIOUS_RANGES_CACHE = dict()
 
     @staticmethod
     @lru_cache(maxsize=8192)
@@ -37,3 +38,30 @@ class UnicodeRangeIdentify:
         if len(letter) != 1:
             raise IOError('Trying to determine accentuated state of multiple char <{}>'.format(letter))
         return 192 <= ord(letter) <= 383
+
+    @staticmethod
+    @lru_cache(maxsize=512)
+    def get_range_id(range_name):
+        return UNICODE_RANGES_NAMES.index(range_name)
+
+    @staticmethod
+    def unravel_suspicious_ranges(str_len, encountered_unicode_range_occurrences):
+        """
+        :param dict encountered_unicode_range_occurrences:
+        :return:
+        """
+
+        items = encountered_unicode_range_occurrences.items()
+        s_ = 0
+
+        for k, v in items:
+            k_ = k.lower()
+            if (
+                    'latin' not in k_ and 'general punctuation' not in k_ and 'halfwidth and fullwidth forms' not in k_ and 'symbols and punctuation' not in k_ and 'cjk' not in k_) or 'latin extended' in k_ or 'latin-1 supplement' in k_:
+                if v / str_len < 0.09:
+                    if len(encountered_unicode_range_occurrences.keys()) <= 2 and 'latin-1 supplement' in k_:
+                        continue
+                    # print('Suspicous ranges added', k, v)
+                    s_ += v if 'geometric shapes' not in k_ else v * 10
+
+        return s_
