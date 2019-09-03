94575db3991c969bacc9ea689da9cf6be1facc72
TAHRI Ahmed R <nyuubi.10@gmail.com>
2019-09-03 22:07:41 +0200

    bump 0.2.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ URL = 'https://github.com/ousret/charset_normalizer'
 EMAIL = 'ahmed.tahri@cloudnursery.dev'
 AUTHOR = 'Ahmed TAHRI @Ousret'
 REQUIRES_PYTHON = '>=3.4.0'
-VERSION = '0.2.0'
+VERSION = '0.2.1'
 
 REQUIRED = [
     'cached_property',
