5f749657229c60da303bff5ca975783054f1bb48
TAHRI Ahmed R <nyuubi.10@gmail.com>
2019-09-06 22:03:38 +0200

    Add legacy method detect as chardet does

--- charset_normalizer/__init__.py
+++ charset_normalizer/__init__.py
@@ -3,3 +3,4 @@ from charset_normalizer.normalizer import CharsetNormalizerMatches, CharsetNorma
 from charset_normalizer.unicode import UnicodeRangeIdentify
 from charset_normalizer.probe_chaos import ProbeChaos
 from charset_normalizer.probe_coherence import ProbeCoherence
+from charset_normalizer.legacy import detect
---
+++ charset_normalizer/legacy.py
@@ -0,0 +1,26 @@
+from charset_normalizer.normalizer import CharsetNormalizerMatches as CnM
+
+
+def detect(byte_str):
+    """
+    chardet/cchardet legacy method
+    Detect the encoding of the given byte string.
+
+    :param byte_str:     The byte sequence to examine.
+    :type byte_str:      ``bytes`` or ``bytearray``
+    :rtype: dict
+    """
+    if not isinstance(byte_str, bytearray):
+        if not isinstance(byte_str, bytes):
+            raise TypeError('Expected object of type bytes or bytearray, got: '
+                            '{0}'.format(type(byte_str)))
+        else:
+            byte_str = bytearray(byte_str)
+
+    r = CnM.from_bytes(byte_str).best().first()
+
+    return {
+        'encoding': r.encoding,
+        'language': r.language if r.language != 'Unknown' else '',
+        'confidence': 1. - r.chaos
+    } if r is not None else None
