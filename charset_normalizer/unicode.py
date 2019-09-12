0b8c445b0612261ca1a6261832313d9ad429a598
TAHRI Ahmed R <nyuubi.10@gmail.com>
2019-09-12 18:56:45 +0200

    Add methods to UnicodeRangeIdentify

--- charset_normalizer/unicode.py
+++ charset_normalizer/unicode.py
@@ -1,6 +1,7 @@
 # coding: utf-8
-from charset_normalizer.constant import UNICODE_RANGES_ZIP, UNICODE_RANGES_NAMES
 from functools import lru_cache
+
+from charset_normalizer.constant import UNICODE_RANGES_ZIP, UNICODE_RANGES_NAMES, UNICODE_SECONDARY_RANGE_KEYWORD
 
 
 class UnicodeRangeIdentify:
@@ -52,7 +53,7 @@ class UnicodeRangeIdentify:
         :param str letter:
         :return:
         """
-        return 'Latin' in UnicodeRangeIdentify.find_letter_type(letter)
+        return 'Latin' in (UnicodeRangeIdentify.find_letter_type(letter) or '')
 
     @staticmethod
     @lru_cache(maxsize=8192)
@@ -65,9 +66,10 @@ class UnicodeRangeIdentify:
         if letter.isspace():
             return True
         r_name = UnicodeRangeIdentify.find_letter_type(letter)
-        return "Punctuation" in r_name or \
-               'Forms' in r_name or \
-               letter in 'º¯—–‒‐⁃«‹?!;.:^$*»£¹¿~ª؟©±¡{}[]|¼½¾⅕⅙⅛™℠‼⁇❝❞¶⁋√↑↓�'
+        return r_name is not None and \
+               ("Punctuation" in r_name or
+               'Forms' in r_name or
+               letter in 'º¯—–‒‐⁃«‹?!;.:^$*»£¹¿~ª؟©±¡{}[]|¼½¾⅕⅙⅛™℠‼⁇❝❞¶⁋√↑↓�¤`')
 
     @staticmethod
     @lru_cache(maxsize=8192)
@@ -77,7 +79,7 @@ class UnicodeRangeIdentify:
         :param str letter:
         :return:
         """
-        return 'CJK' in UnicodeRangeIdentify.find_letter_type(letter)
+        return 'CJK' in (UnicodeRangeIdentify.find_letter_type(letter) or '')
 
     @staticmethod
     def unravel_suspicious_ranges(str_len, encountered_unicode_range_occurrences):
@@ -110,13 +112,15 @@ class UnicodeRangeIdentify:
         :param str range_name_b:
         :return:
         """
+        if range_name_a is None or range_name_b is None:
+            return True
 
         dec_range_name_a, dec_range_name_b = range_name_a.split(), range_name_b.split()
 
         if range_name_a == range_name_b:
             return False
 
-        if 'Latin' in range_name_a or 'Latin' in range_name_b:
+        if 'Latin' in range_name_a and 'Latin' in range_name_b:
             return False
 
         for el in dec_range_name_a:
@@ -130,3 +134,110 @@ class UnicodeRangeIdentify:
             return False
 
         return True
+
+    @staticmethod
+    def classification(word):
+        """
+        :param str word:
+        :return:
+        """
+        cla_ = dict()
+
+        for el in word:
+            if el.isspace():
+                raise IOError('Classification should not be invoked with sentences !')
+            u_name = UnicodeRangeIdentify.find_letter_type(el)
+            if u_name is None:
+                u_name = 'Unknown'
+            if u_name not in cla_:
+                cla_[u_name] = 0
+            cla_[u_name] += 1
+
+        return cla_
+
+    @staticmethod
+    @lru_cache(maxsize=512)
+    def is_range_secondary(u_range):
+        """
+        :param str u_range:
+        :return:
+        """
+        try:
+            UnicodeRangeIdentify.get_range_id(u_range)
+        except ValueError:
+            return True
+
+        for keyword in UNICODE_SECONDARY_RANGE_KEYWORD:
+            if keyword in u_range:
+                return True
+
+        return False
+
+    @staticmethod
+    def part_punc(word):
+        """
+        Determine how much of the word is composed of punc sign
+        :param str word:
+        :return:
+        """
+        return [UnicodeRangeIdentify.is_punc(el) for el in word].count(True) / len(word)
+
+    @staticmethod
+    def part_accent(word):
+        """
+        Determine how much of the word is composed of accentuated letter
+        :param word:
+        :return:
+        """
+        return [UnicodeRangeIdentify.is_accentuated(el) for el in word].count(True) / len(word)
+
+    @staticmethod
+    def word_to_range_list(word):
+        """
+
+        :param str word:
+        :return:
+        """
+        return [UnicodeRangeIdentify.find_letter_type(el) for el in word]
+
+    @staticmethod
+    def word_to_range_continue(word):
+        """
+
+        :param str word:
+        :return:
+        """
+        l_ = list()
+
+        for el in word:
+            u_name = UnicodeRangeIdentify.find_letter_type(el)
+            if len(l_) == 0:
+                l_.append(
+                    (
+                        u_name,
+                        1
+                    )
+                )
+            else:
+                if UnicodeRangeIdentify.is_suspiciously_successive_range(u_name, l_[-1][0]) is True:
+                    l_.append(
+                        (
+                            u_name,
+                            1
+                        )
+                    )
+                else:
+                    l_[-1] = (
+                        u_name,
+                        l_[-1][1]+1
+                    )
+
+        return l_
+
+    @staticmethod
+    def part_lonely_range(word):
+        """
+        :param str word:
+        :return:
+        """
+        return [u_occ_cont == 1 for u_name, u_occ_cont in UnicodeRangeIdentify.word_to_range_continue(word)].count(True) / len(word)
