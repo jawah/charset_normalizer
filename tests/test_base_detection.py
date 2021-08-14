b639a3cad4fa7005fc8add8fc455ad49349f6c26
Ahmed TAHRI <ahmed.tahri@cloudnursery.dev>
2021-08-14 23:05:58 +0200

    :art: Better tests, more comprehensible (detection engine)

---
+++ tests/test_base_detection.py
@@ -0,0 +1,123 @@
+import unittest
+
+from charset_normalizer.api import from_bytes
+from charset_normalizer.constant import TOO_BIG_SEQUENCE
+
+import pytest
+
+
+def test_empty():
+    best_guess = from_bytes(b'').best()
+
+    assert best_guess is not None, "Empty bytes payload SHOULD NOT return None"
+    assert best_guess.encoding == "utf_8", "Empty bytes payload SHOULD be guessed as UTF-8 (arbitrary)"
+    assert len(best_guess.alphabets) == 0, ""
+
+
+@pytest.mark.parametrize(
+    "payload, expected_encoding",
+    [
+        (b'\xfe\xff', 'utf_16'),
+        ('\uFEFF'.encode('gb18030'), 'gb18030'),
+        (b'\xef\xbb\xbf', 'utf_8'),
+        ("".encode('utf_32'), "utf_32")
+    ]
+)
+def test_empty_but_with_bom_or_sig(payload, expected_encoding):
+    best_guess = from_bytes(payload).best()
+
+    assert best_guess is not None, "Empty detection but with SIG/BOM has failed!"
+    assert best_guess.encoding == expected_encoding, "Empty detection but with SIG/BOM is wrongly detected!"
+    assert best_guess.raw == payload, "The RAW property should contain the original payload given for detection."
+    assert best_guess.byte_order_mark is True, "The BOM/SIG property should return True"
+    assert str(best_guess) == "", "The cast to str SHOULD be empty"
+
+
+@pytest.mark.parametrize(
+    "payload, expected_encoding",
+    [
+        ((u'\uFEFF' + '我没有埋怨，磋砣的只是一些时间。').encode('gb18030'), "gb18030",),
+        ('我没有埋怨，磋砣的只是一些时间。'.encode('utf_32'), "utf_32",),
+        ('我没有埋怨，磋砣的只是一些时间。'.encode('utf_8_sig'), "utf_8",),
+
+    ]
+)
+def test_content_with_bom_or_sig(payload, expected_encoding):
+    best_guess = from_bytes(payload).best()
+
+    assert best_guess is not None, "Detection but with SIG/BOM has failed!"
+    assert best_guess.encoding == expected_encoding, "Detection but with SIG/BOM is wrongly detected!"
+    assert best_guess.byte_order_mark is True, "The BOM/SIG property should return True"
+
+
+@pytest.mark.parametrize(
+    "payload",
+    [
+        b"AbAdZ pOoooOlDl mmlDoDkA lldDkeEkddA mpAlkDF",
+        b"g4UsPJdfzNkGW2jwmKDGDilKGKYtpF2X.mx3MaTWL1tL7CNn5U7DeCcodKX7S3lwwJPKNjBT8etY",
+        b'{"token": "g4UsPJdfzNkGW2jwmKDGDilKGKYtpF2X.mx3MaTWL1tL7CNn5U7DeCcodKX7S3lwwJPKNjBT8etY"}',
+        b"81f4ab054b39cb0e12701e734077d84264308f5fc79494fc5f159fa2ebc07b73c8cc0e98e009664a20986706f90146e8eefcb929ce1f74a8eab21369fdc70198",
+        b"{}",
+    ]
+)
+def test_obviously_ascii_content(payload):
+    best_guess = from_bytes(payload).best()
+
+    assert best_guess is not None, "Dead-simple ASCII detection has failed!"
+    assert best_guess.encoding == "ascii", "Dead-simple ASCII detection is wrongly detected!"
+
+
+@pytest.mark.parametrize(
+    "payload",
+    [
+        '\u020d\x1b'.encode('utf-8'),
+        'h\xe9llo world!\n'.encode('utf_8'),
+        '我没有埋怨，磋砣的只是一些时间。'.encode('utf_8'),
+        'Bсеки човек има право на образование. Oбразованието трябва да бъде безплатно, поне що се отнася до началното и основното образование.'.encode('utf_8'),
+        'Bсеки човек има право на образование.'.encode('utf_8'),
+        "(° ͜ʖ °), creepy face, smiley 😀".encode("utf_8"),
+        """["Financiën", "La France"]""".encode("utf_8"),
+        "Qu'est ce que une étoile?".encode("utf_8"),
+        """<?xml ?><c>Financiën</c>""".encode("utf_8")
+    ]
+)
+def test_obviously_utf8_content(payload):
+    best_guess = from_bytes(payload).best()
+
+    assert best_guess is not None, "Dead-simple UTF-8 detection has failed!"
+    assert best_guess.encoding == "utf_8", "Dead-simple UTF-8 detection is wrongly detected!"
+
+
+class TestBytes(unittest.TestCase):
+
+    def test_alphabets_property_undefined_range(self):
+        payload = b'\xef\xbb\xbf\xf0\x9f\xa9\xb3'
+
+        r = from_bytes(payload)
+
+        self.assertEqual(
+            r.best().encoding,
+            "utf_8"
+        )
+
+        self.assertEqual(
+            r.best().alphabets,
+            []
+        )
+
+    def test_empty_str_with_large_sig_utf8(self):
+        r = from_bytes(b'\xef\xbb\xbf' + (b'0' * TOO_BIG_SEQUENCE)).best()
+
+        self.assertIsNotNone(r)
+        self.assertEqual(
+            '0' * TOO_BIG_SEQUENCE,
+            str(r)
+        )
+        self.assertEqual(
+            "utf_8",
+            r.encoding
+        )
+        self.assertEqual(
+            TOO_BIG_SEQUENCE + 3,
+            len(r.raw)
+        )
---
+++ tests/test_full_detection.py
@@ -0,0 +1,42 @@
+from charset_normalizer.api import from_path
+import pytest
+
+
+@pytest.mark.parametrize(
+    "input_data_file, expected_charset, expected_language",
+    [
+        ('sample.1.ar.srt', 'cp1256', 'Arabic'),
+        ('sample.1.fr.srt', 'cp1252', 'French'),
+        ('sample.1.gr.srt', 'cp1253', 'Greek'),
+        ('sample.1.he.srt', 'cp1255', 'Hebrew'),
+        ('sample.1.hi.srt', 'ascii', 'English'),
+        ('sample.1.ru.srt', 'cp1251', 'Russian'),
+        ('sample.1.tu.srt', 'cp1254', 'Turkish'),
+        ('sample.2.ar.srt', 'cp1256', 'Arabic'),
+        ('sample.3.ar.srt', 'utf_8', 'Arabic'),
+        ('sample.4.ar.srt', 'cp1256', 'Arabic'),
+        ('sample.5.ar.srt', 'utf_8', 'Arabic'),
+        ('sample-chinese.txt', 'big5', 'Classical Chinese'),
+        ('sample-greek.txt', 'cp1253', 'Greek'),
+        ('sample-greek-2.txt', 'cp1253', 'Greek'),
+        ('sample-hebrew-2.txt', 'cp1255', 'Hebrew'),
+        ('sample-hebrew-3.txt', 'cp1255', 'Hebrew'),
+        ('sample-bulgarian.txt', 'utf_8', 'Bulgarian'),
+        ('sample-english.bom.txt', 'utf_8', 'English'),
+        ('sample-spanish.txt', 'utf_8', 'Spanish'),
+        ('sample-korean.txt', 'cp949', 'Korean'),
+        ('sample-turkish.txt', 'cp1254', 'Turkish'),
+        ('sample-russian-2.txt', 'utf_8', 'Russian'),
+        ('sample-russian.txt', 'mac_cyrillic', 'Russian'),
+    ]
+)
+def test_elementary_detection(
+    input_data_file: str,
+    expected_charset: str,
+    expected_language: str,
+):
+    best_guess = from_path("./data/{}".format(input_data_file)).best()
+
+    assert best_guess is not None, "Elementary detection has failed upon '{}'".format(input_data_file)
+    assert best_guess.language == expected_language, "Elementary charset detection has failed upon '{}'".format(input_data_file)
+    assert best_guess.language == expected_language, "Elementary language detection has failed upon '{}'".format(input_data_file)
