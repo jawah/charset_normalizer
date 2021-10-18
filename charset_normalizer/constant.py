5dd6a5bad73ac62ddce4ac01bff473d55728b1cb
Adrien Barbaresi <barbaresi@bbaw.de>
2021-10-18 14:34:13 +0200

    use constant to limit LRU size in get_target_features()

--- charset_normalizer/constant.py
+++ charset_normalizer/constant.py
@@ -4,6 +4,8 @@ from encodings.aliases import aliases
 from re import IGNORECASE, compile as re_compile
 from typing import Dict, List, Set, Union
 
+from .assets import FREQUENCIES
+
 # Contain for each eligible encoding a list of/item bytes SIG/BOM
 ENCODING_MARKS = OrderedDict(
     [
@@ -494,3 +496,5 @@ KO_NAMES = {"johab", "cp949", "euc_kr"}  # type: Set[str]
 ZH_NAMES = {"big5", "cp950", "big5hkscs", "hz"}  # type: Set[str]
 
 NOT_PRINTABLE_PATTERN = re_compile(r"[0-9\W\n\r\t]+")
+
+FREQUENCIES_SIZE = len(FREQUENCIES)
