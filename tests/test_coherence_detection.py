c6fa3695d7b0b33f98b5803a45d56cfe64960264
Ahmed TAHRI <ahmed.tahri@cloudnursery.dev>
2021-10-23 22:53:14 +0200

    :heavy_check_mark: Add test cases

--- tests/test_coherence_detection.py
+++ tests/test_coherence_detection.py
@@ -1,5 +1,5 @@
 import pytest
-from charset_normalizer.cd import encoding_languages, mb_encoding_languages, is_multi_byte_encoding
+from charset_normalizer.cd import encoding_languages, mb_encoding_languages, is_multi_byte_encoding, get_target_features
 
 
 @pytest.mark.parametrize(
@@ -21,3 +21,21 @@ def test_infer_language_from_cp(iana_encoding, expected_languages):
 
     for expected_language in expected_languages:
         assert expected_language in languages, "Wrongly detected language for given code page"
+
+
+@pytest.mark.parametrize(
+    "language, expected_have_accents, expected_pure_latin",
+    [
+        ("English", False, True),
+        ("French", True, True),
+        ("Hebrew", False, False),
+        ("Arabic", False, False),
+        ("Vietnamese", True, True),
+        ("Turkish", True, True)
+    ]
+)
+def test_target_features(language, expected_have_accents, expected_pure_latin):
+    target_have_accents, target_pure_latin = get_target_features(language)
+
+    assert target_have_accents is expected_have_accents
+    assert target_pure_latin is expected_pure_latin
