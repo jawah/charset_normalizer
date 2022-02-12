3d5b4c244ae8cf4364a82ad3318645e3da842bfe
Ahmed TAHRI <ahmed.tahri@cloudnursery.dev>
2022-02-12 15:16:48 +0100

    :wrench: Force the dir/listening (dataset) with specific order in additionals tests

--- bin/serve.py
+++ bin/serve.py
@@ -13,7 +13,7 @@ def read_file(path):
 def read_targets():
     return jsonify(
         [
-            el.replace("./char-dataset", "/raw").replace("\\", "/") for el in glob("./char-dataset/**/*")
+            el.replace("./char-dataset", "/raw").replace("\\", "/") for el in sorted(glob("./char-dataset/**/*"))
         ]
     )
 
