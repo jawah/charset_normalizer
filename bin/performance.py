a6519418f3ad48ed4013edb82ec4efcbca50ecef
deedy5 <65482418+deedy5@users.noreply.github.com>
2022-05-05 12:46:02 +0300

    Charset-Normalizer / Chardet: performance comparison (#186)
    * Update performance.py
    
    Charset-Normalizer / Chardet: Performance Сomparison

--- bin/performance.py
+++ bin/performance.py
@@ -82,6 +82,13 @@ def performance_compare(arguments):
     print("   --> 99th: " + str(charset_normalizer_99p) + "s")
     print("   --> 95th: " + str(charset_normalizer_95p) + "s")
     print("   --> 50th: " + str(charset_normalizer_50p) + "s")
+    
+    print("------------------------------")
+    print("--> Charset-Normalizer / Chardet: Performance Сomparison")
+    print("   --> Avg: " + str(round(((chardet_avg_delay / charset_normalizer_avg_delay - 1) * 100), 2)) + "%")        
+    print("   --> 99th: " + str(round(((chardet_99p / charset_normalizer_99p - 1) * 100), 2)) + "%")
+    print("   --> 95th: " + str(round(((chardet_95p / charset_normalizer_95p - 1) * 100), 2)) + "%")
+    print("   --> 50th: " + str(round(((chardet_50p / charset_normalizer_50p - 1) * 100), 2)) + "%")
 
     return 0 if chardet_avg_delay > charset_normalizer_avg_delay and chardet_99p > charset_normalizer_99p else 1
 
