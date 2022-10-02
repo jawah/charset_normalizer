32cbafeef71ef6988f6815dea51fb48820c6cc55
Ahmed TAHRI <ahmed.tahri@cloudnursery.dev>
2022-10-02 13:56:56 +0200

    :wrench: run_checks.sh adjust black target lvl py36

--- bin/run_checks.sh
+++ bin/run_checks.sh
@@ -8,7 +8,7 @@ fi
 set -x
 
 ${PREFIX}pytest
-${PREFIX}black --check --diff --target-version=py35 charset_normalizer
+${PREFIX}black --check --diff --target-version=py36 charset_normalizer
 ${PREFIX}flake8 charset_normalizer
 ${PREFIX}mypy charset_normalizer
 ${PREFIX}isort --check --diff charset_normalizer
