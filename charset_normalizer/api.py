39b6f5c16ff90f3786bd92a3d9b54919b2fe2fad
TAHRI Ahmed R. <Ousret@users.noreply.github.com>
2024-09-25 14:24:09 +0200

    :wrench: improve the detector general reliability (#532)
    Issues (#520) (#509) (#498) (#407)

--- charset_normalizer/api.py
+++ charset_normalizer/api.py
@@ -221,16 +221,20 @@ def from_bytes(
         try:
             if is_too_large_sequence and is_multi_byte_decoder is False:
                 str(
-                    sequences[: int(50e4)]
-                    if strip_sig_or_bom is False
-                    else sequences[len(sig_payload) : int(50e4)],
+                    (
+                        sequences[: int(50e4)]
+                        if strip_sig_or_bom is False
+                        else sequences[len(sig_payload) : int(50e4)]
+                    ),
                     encoding=encoding_iana,
                 )
             else:
                 decoded_payload = str(
-                    sequences
-                    if strip_sig_or_bom is False
-                    else sequences[len(sig_payload) :],
+                    (
+                        sequences
+                        if strip_sig_or_bom is False
+                        else sequences[len(sig_payload) :]
+                    ),
                     encoding=encoding_iana,
                 )
         except (UnicodeDecodeError, LookupError) as e:
--- charset_normalizer/version.py
+++ charset_normalizer/version.py
@@ -2,5 +2,5 @@
 Expose version
 """
 
-__version__ = "3.3.2"
+__version__ = "3.3.3"
 VERSION = __version__.split(".")
