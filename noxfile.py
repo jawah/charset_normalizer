b3f4f45f5d56b52638a2cc7b49559e506f2a94cd
Ahmed TAHRI <tahri.ahmed@proton.me>
2025-01-26 17:26:12 +0100

    🚨 fix CPython 3.14 deprecation warning for argparse.FileType
    We decided to backport argparse.FileType as no solid reason emerge for us to stop using it. We properly close the fp afterward.
    
    Close #591

--- noxfile.py
+++ noxfile.py
@@ -52,7 +52,9 @@ def test_impl(
     )
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "pypy"])
+@nox.session(
+    python=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "3.14", "pypy"]
+)
 def test(session: nox.Session) -> None:
     test_impl(session)
 
