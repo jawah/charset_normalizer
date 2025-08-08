8c4602b1362d79ea3dc60cdf55d6320d23ab2183
Ahmed TAHRI <tahri.ahmed@proton.me>
2025-08-08 23:45:43 +0100

    :wrench: allow conditional installation of mypy(c) in build phase through custom build backend

--- setup.py
+++ setup.py
@@ -2,21 +2,20 @@
 from __future__ import annotations
 
 import os
-import sys
 
 from setuptools import setup
 
 USE_MYPYC = False
 
-if len(sys.argv) > 1 and sys.argv[1] == "--use-mypyc":
-    sys.argv.pop(1)
-    USE_MYPYC = True
-elif os.getenv("CHARSET_NORMALIZER_USE_MYPYC", None) == "1":
+if os.getenv("CHARSET_NORMALIZER_USE_MYPYC", None) == "1":
     USE_MYPYC = True
 
-if USE_MYPYC:
+try:
     from mypyc.build import mypycify
+except ImportError:
+    mypycify = None  # type: ignore[assignment]
 
+if USE_MYPYC and mypycify is not None:
     MYPYC_MODULES = mypycify(
         [
             "src/charset_normalizer/md.py",
